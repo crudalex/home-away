9fd12151c1e649b0feefa8974e76590e0fed1a90
Alex Lam <Alex_TW_Lam@manulife.com>
2024-11-07 21:20:20 +0800

    Refactor Create Profile

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,18 +1,30 @@
+import { SubmitButton } from "@/components/form/Buttons";
+import FormContainer from "@/components/form/FormContainer";
+import FormInput from "@/components/form/FormInput";
+import { Sub } from "@radix-ui/react-dropdown-menu";
 import React from "react";
 
-const createProfileAction = async (formData: FormData) => {
+const createProfileAction = async (preState: any, formData: FormData) => {
   "use server";
   const firstName = formData.get("firstName");
-  console.log(firstName);
+  if (firstName !== "shakeAndBake") return { message: "There was an error..." };
+  return { message: "Profile created" };
 };
 
 function CreateProfilePage() {
   return (
     <section>
       <h1 className="text-2xl font-semibold mb-8 capitalize">
-        new user
-        <div className="border p-8 round-md max-w-lg ">
-          <form action={createProfileAction}></form>
+        New User
+        <div className="border p-8 round-md">
+          <FormContainer action={createProfileAction}>
+            <div className="grid md:grid-cols-2 gap-4 mt-4">
+              <FormInput type="text" name="firstName" label="First Name" />
+              <FormInput type="text" name="lastName" label="Last Name" />
+              <FormInput type="text" name="username" label="Username" />
+            </div>
+            <SubmitButton text="Create Profile" className="mt-8" />
+          </FormContainer>
         </div>
       </h1>
     </section>
