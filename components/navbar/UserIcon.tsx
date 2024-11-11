46e98b0692b5128e19f69a438c6565d2c6c2d950
Alex Lam <Alex_TW_Lam@manulife.com>
2024-11-11 14:28:01 +0800

    Fetch Profile Image Action

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 import { LuUser2 } from "react-icons/lu";
+import { fetchProfileImage } from "@/utils/actions";
 
-function UserIcon() {
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+  if (profileImage) {
+    return (
+      <img
+        src={profileImage}
+        className="w-6 h-6 bg-primary rounded-full text-white"
+      />
+    );
+  }
   return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 
