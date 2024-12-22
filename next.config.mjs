8711345d46ec6fa28971363a6175955d58b52596
Alex Lam <crudalex@gmail.com>
2024-12-22 21:58:44 +0800

    Image Input Container

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[
+      {
+        protocol: 'https', 
+        hostname: 'img.clerk.com'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
--- utils/actions.ts
+++ utils/actions.ts
@@ -98,3 +98,10 @@ export const updateProfileAction: actionFunction = async (
     return renderError(error);
   }
 };
+
+export const updateProfileImageAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  return { message: "Profile image updated successfully" };
+};
